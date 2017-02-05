1ad11ce9b14ae43ad9398a595fb2e383a923a480
mccrodp <info@opensourceopenmind.ninja>
2017-02-05 00:02:32 +0000

    Issue #2848516 by pifagor: Change t() to $this->t().

--- src/Form/SettingsForm.php
+++ src/Form/SettingsForm.php
@@ -85,30 +85,30 @@ class SettingsForm extends ConfigFormBase {
     // Global email notifications on/off checkbox.
     $form['email_notify'] = [
       '#type' => 'checkbox',
-      '#title' => t('Message Private Email Notifications'),
+      '#title' => $this->t('Message Private Email Notifications'),
       '#default_value' => $config->get('email_notify'),
-      '#description' => t('Global On / Off checkbox for emails notifying users of a new private message'),
+      '#description' => $this->t('Global On / Off checkbox for emails notifying users of a new private message'),
     ];
 
     // Local action links on/off checkbox.
     $form['message_add_local_action'] = [
       '#type' => 'checkbox',
-      '#title' => t('Disable Create a New Message local action links'),
+      '#title' => $this->t('Disable Create a New Message local action links'),
       '#default_value' => $config->get('message_add_local_action'),
-      '#description' => t('Disable local action links to create new message on user pages.'),
+      '#description' => $this->t('Disable local action links to create new message on user pages.'),
     ];
 
     // Role based message create limit on/off checkbox.
     $form['message_limit'] = [
       '#type' => 'checkbox',
-      '#title' => t('Limit Message Create By Role'),
+      '#title' => $this->t('Limit Message Create By Role'),
       '#default_value' => $config->get('message_limit'),
-      '#description' => t('Impose a message creation limit per interval. Users with multiple roles, get the highest limit from these roles'),
+      '#description' => $this->t('Impose a message creation limit per interval. Users with multiple roles, get the highest limit from these roles'),
     ];
 
     // Conditional fieldset for all message limitation settings.
     $form['interval_limit'] = [
-      '#title' => t('Message interval limits'),
+      '#title' => $this->t('Message interval limits'),
       '#type' => 'fieldset',
       '#collapsible' => TRUE,
       '#collapsed' => TRUE,
@@ -123,24 +123,24 @@ class SettingsForm extends ConfigFormBase {
     $form['interval_limit'][MESSAGE_PRIVATE_DEFAULT_INDEX] = [
       '#type' => 'fieldset',
       '#title' => 'Default limit',
-      '#description' => t('Applies to all roles with blank entries below'),
+      '#description' => $this->t('Applies to all roles with blank entries below'),
       '#collapsible' => TRUE,
       '#collapsed' => FALSE,
     ];
 
     $form['interval_limit'][MESSAGE_PRIVATE_DEFAULT_INDEX]['default_limit'] = [
       '#type' => 'textfield',
-      '#title' => t('Limit'),
+      '#title' => $this->t('Limit'),
       '#default_value' => $config->get('default_limit'),
-      '#description' => t('Enter a message limit') . ' ' . MESSAGE_PRIVATE_MESSAGE_LIMIT_MIN . ' - ' . MESSAGE_PRIVATE_MESSAGE_LIMIT_MAX,
+      '#description' => $this->t('Enter a message limit') . ' ' . MESSAGE_PRIVATE_MESSAGE_LIMIT_MIN . ' - ' . MESSAGE_PRIVATE_MESSAGE_LIMIT_MAX,
     ];
 
 
      $form['interval_limit'][MESSAGE_PRIVATE_DEFAULT_INDEX]['default_interval'] = [
        '#type' => 'textfield',
-       '#title' => t('Interval'),
+       '#title' => $this->t('Interval'),
        '#default_value' => $config->get('default_interval'),
-       '#description' => t('Enter an interval in minutes') . ' ' . MESSAGE_PRIVATE_MESSAGE_INTERVAL_MIN . ' - ' . MESSAGE_PRIVATE_MESSAGE_INTERVAL_MAX,
+       '#description' => $this->t('Enter an interval in minutes') . ' ' . MESSAGE_PRIVATE_MESSAGE_INTERVAL_MIN . ' - ' . MESSAGE_PRIVATE_MESSAGE_INTERVAL_MAX,
      ];
 
 
@@ -159,16 +159,16 @@ class SettingsForm extends ConfigFormBase {
 
       $form['interval_limit'][$id][$limit_name] = array(
         '#type' => 'textfield',
-        '#title' => t('Limit'),
+        '#title' => $this->t('Limit'),
         '#default_value' => $config->get($limit_name),
-        '#description' => t('Enter a message limit') . ' ' . MESSAGE_PRIVATE_MESSAGE_LIMIT_MIN . ' - ' . MESSAGE_PRIVATE_MESSAGE_LIMIT_MAX,
+        '#description' => $this->t('Enter a message limit') . ' ' . MESSAGE_PRIVATE_MESSAGE_LIMIT_MIN . ' - ' . MESSAGE_PRIVATE_MESSAGE_LIMIT_MAX,
       );
 
       $form['interval_limit'][$id][$interval_name] = array(
         '#type' => 'textfield',
-        '#title' => t('Interval'),
+        '#title' => $this->t('Interval'),
         '#default_value' => $config->get($interval_name),
-        '#description' => t('Enter an interval in minutes') . ' ' . MESSAGE_PRIVATE_MESSAGE_INTERVAL_MIN . ' - ' . MESSAGE_PRIVATE_MESSAGE_INTERVAL_MAX,
+        '#description' => $this->t('Enter an interval in minutes') . ' ' . MESSAGE_PRIVATE_MESSAGE_INTERVAL_MIN . ' - ' . MESSAGE_PRIVATE_MESSAGE_INTERVAL_MAX,
       );
     }
 
@@ -254,19 +254,19 @@ class SettingsForm extends ConfigFormBase {
       // Check is numeric and between the boundaries.
       if (!ctype_digit($limit) || $limit > MESSAGE_PRIVATE_MESSAGE_LIMIT_MAX || $limit < MESSAGE_PRIVATE_MESSAGE_LIMIT_MIN) {
         $form_state->setErrorByName($limit_element,
-          t('Enter a numerical message limit between') . ' '
+          $this->t('Enter a numerical message limit between') . ' '
           . MESSAGE_PRIVATE_MESSAGE_LIMIT_MIN . ' - ' . MESSAGE_PRIVATE_MESSAGE_LIMIT_MAX . '.');
       }
       // Check is numeric and between the boundaries.
       if (!ctype_digit($interval) || $interval > MESSAGE_PRIVATE_MESSAGE_INTERVAL_MAX || $interval < MESSAGE_PRIVATE_MESSAGE_INTERVAL_MIN) {
          $form_state->setErrorByName($interval_element,
-          t('Enter a numerical interval in minutes between') . ' '
+           $this->t('Enter a numerical interval in minutes between') . ' '
           . MESSAGE_PRIVATE_MESSAGE_INTERVAL_MIN . ' - ' . MESSAGE_PRIVATE_MESSAGE_INTERVAL_MAX . '.');
       }
     }
     elseif (!empty($limit) || !empty($interval)) {
       // Show error if only 1 textfield is set in each fieldset.
-      $form_state->setErrorByName($limit_element, t('Both a limit and interval value are required.'));
+      $form_state->setErrorByName($limit_element, $this->t('Both a limit and interval value are required.'));
       $form_state->setErrorByName($interval_element);
     }
   }
