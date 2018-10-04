b923cd0aafd42941ab38ed7c6166e309425600ed
Claudiu Cristea <clau.cristea@gmail.com>
2018-10-04 10:46:28 +0300

    test

--- scripts/travis-ci/run-tests.sh
+++ scripts/travis-ci/run-tests.sh
@@ -14,17 +14,17 @@ TEST_DIRS=($MODULE_DIR/tests)
 
 case "$1" in
     PHP_CodeSniffer)
-        cd $MODULE_DIR
+        cd ${MODULE_DIR}/
         ./vendor/bin/phpcs
         exit $?
         ;;
     *)
         mysql_to_ramdisk
-        cd $DRUPAL_DIR
+        cd ${DRUPAL_DIR}/
         EXIT=0
         for i in ${TEST_DIRS[@]}; do
             echo "Executing tests in $i"
-            $TRAVIS_BUILD_DIR/vendor/bin/phpunit -c $DRUPAL_DIR/core/phpunit.xml.dist $i || EXIT=1
+            ${TRAVIS_BUILD_DIR}/vendor/bin/phpunit -c ${DRUPAL_DIR}/core/phpunit.xml.dist $i || EXIT=1
         done
         exit $EXIT
 esac
